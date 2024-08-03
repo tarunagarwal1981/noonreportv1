b150b4cac7341d7c460dfb6363f78937cfe89164
tarunagarwal1981 <49944511+tarunagarwal1981@users.noreply.github.com>
2024-08-03 12:17:09 +0530

    Update Sections & Fields.py

--- pages/Sections & Fields.py
+++ pages/Sections & Fields.py
@@ -1,3 +1,9 @@
+import streamlit as st
+import pandas as pd
+from datetime import datetime, time
+
+st.set_page_config(layout="wide", page_title="Noon Reporting Portal")
+
 def main():
     st.title("Noon Reporting Portal")
 
