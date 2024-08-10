6a424f19f77ad25154bc13cfe5eee191d0effc91
tarunagarwal1981 <49944511+tarunagarwal1981@users.noreply.github.com>
2024-08-10 16:41:04 +0530

    Update test.py

--- pages/test.py
+++ pages/test.py
@@ -17,37 +17,32 @@ def main():
     with col3:
         st.text("Vessel Type: Tanker")  # Random value
 
-    st.markdown("<h2 style='text-align: center;'>Departure Report Selection</h2>", unsafe_allow_html=True)
+    st.markdown("<h2 style='text-align: center;'>COSP Report Selection</h2>", unsafe_allow_html=True)
     
     # Arrange the noon report checkboxes in rows of three
-    col1, col2, col3, col4, col5 = st.columns(5)
+    col1, col2, col3, col4 = st.columns(4)
     with col1:
-        noon_at_portshift = st.checkbox("Departure for Berth Shifting")
-    with col2:
         noon_at_port = st.checkbox("Departure Port")
+    with col2:
+        noon_at_anchor = st.checkbox("Departure from Anchor")
     with col3:
-        noon_at_anchor = st.checkbox("Departure Anchor")
+        noon_at_drifting = st.checkbox("Departure Canal/River")  
     with col4:
-        noon_at_drifting = st.checkbox("Departure Drifting")  
-    with col5:
         noon_at_sts = st.checkbox("Departure STS")    
     
     # Display the relevant form based on the selected checkbox
-    if noon_at_portshift:
-        st.markdown("### Departure for Berth Shifting")
-        display_base_report_form()
     if noon_at_port:
-        st.markdown("### Departure Port")
+        st.markdown("### Departure Port Report")
         display_base_report_form()
     if noon_at_anchor:
-        st.markdown("### Departure Anchor")
+        st.markdown("### Departure from Anchor Report")
         display_base_report_form()
     if noon_at_drifting:
-        st.markdown("### Departure Drifting")
+        st.markdown("### Departure Canal/River")
         display_base_report_form()
     if noon_at_sts:
-        st.markdown("### Departure STS")
-        display_base_report_form()    
+        st.markdown("### Departure STS Report")
+        display_base_report_form()
         
 def display_base_report_form():
     sections = [
@@ -256,7 +251,7 @@ def display_speed_position_and_navigation():
         st.number_input("Time Since Last Report (hours)", min_value=0.0, step=0.1, key="time_since_last_report")
         st.selectbox("Clocks Advanced/Retarded", ["", "Advanced", "Retarded"], key="clocks_change")
         st.number_input("Clocks Changed By (minutes)", min_value=0, step=1, key="clocks_change_minutes")
-        st.time_input("SBE Date Time (Local)", value=datetime.now().time(), key=f"local_time_{uuid.uuid4()}")
+        st.time_input(" COSP Date Time (Local)", value=datetime.now().time(), key=f"local_time_{uuid.uuid4()}")
     with col2:
         st.number_input("Distance Observed (nm)", min_value=0.0, step=0.1, value=0.00, key=f"distance_observed_{uuid.uuid4()}")
         st.number_input("Distance To Go (nm)", min_value=0.0, step=0.1, value=0.00, key=f"distance_togo_{uuid.uuid4()}")
@@ -276,7 +271,7 @@ def display_speed_position_and_navigation():
         st.text_input("Ordered Speed", key=f"speed_order_{uuid.uuid4()}")
         st.text_input("True Slip", key="true_slip")
         st.text_input("Observed Slip", key="obs_slip")
-        st.time_input(" SBE Date Time (UTC)", value=datetime.now().time(), key=f"local_time_{uuid.uuid4()}")
+        st.time_input("COSP Date Time (UTC)", value=datetime.now().time(), key=f"local_time_{uuid.uuid4()}")
 
 def display_custom_speed_position_and_navigation(noon_report_type):
     st.subheader("Speed, Position and Navigation")
