f85c8271880e52dbd7813ad1d9d0337df6f9acfa
tarunagarwal1981 <49944511+tarunagarwal1981@users.noreply.github.com>
2024-08-11 11:09:20 +0530

    Update 2_Arrival.py

--- pages/2_Arrival.py
+++ pages/2_Arrival.py
@@ -125,16 +125,22 @@ def display_voyage_information():
 def display_custom_voyage_information(noon_report_type):
     col1, col2, col3, col4 = st.columns(4)
     with col1:
-        st.text_input("Port Name", key=f"port_name_{uuid.uuid4()}")
-        st.text_input("Port UNLOCODE", key=f"port_unlo_{uuid.uuid4()}")
+        st.text_input("Departure Port", key="voyage_from")
+        st.text_input("UNLOCODE", key="voyage_fromunlo")
+        st.selectbox("Vessel Condition", ["", "Laden", "Ballast"], key=f"vessel_condition_{uuid.uuid4()}")
+        
     with col2:
-        st.selectbox("Voyage Type", ["", "One-way", "Round trip", "STS"], key="voyage_type")
-        st.date_input("ETA", value=datetime.now(), key="eta")
+        st.text_input("Arrival Port", key="voyage_to")
+        st.text_input("UNLOCODE", key="voyage_tounlo")
+        st.selectbox("Voyage Type", ["", "One-way", "Round trip", "STS"], key="voyage_type") 
         
     with col3:
-        st.selectbox("Vessel Condition", ["", "Laden", "Ballast"], key=f"vessel_condition_{uuid.uuid4()}")
-        st.text_input("Charter Type", key="charter_type")
+        st.text_input("Voyage ID", key=f"voyage_id_{uuid.uuid4()}")
+        st.text_input("Segment ID", key=f"segment_id_{uuid.uuid4()}")
+        st.date_input("ETA (Date/Time)", value=datetime.now(), key="eta")
     with col4:
+        st.text_input("Speed Order (CP)", key="speed_order")
+        st.text_input("Charter Type", key="charter_type")
         drydock = st.checkbox("Drydock", key="drydock")
 
 def display_special_events():
@@ -203,7 +209,7 @@ def display_custom_special_events(noon_report_type):
         "Event", "Start Date Time", "Start Lat", "Start Long", "End Date Time",
         "End Lat", "End Long", "Distance Travelled", "Total Consumption", "Consumption Tank Name"
     ]
-
+    
     if 'special_events_df' not in st.session_state:
         default_row = {
             "Event": "Off-hire",
@@ -226,7 +232,14 @@ def display_custom_special_events(noon_report_type):
         column_config={
             "Event": st.column_config.SelectboxColumn(
                 "Event", width="medium",
-                options=["Off-hire", "ECA Transit", "Fuel Changeover"]
+                options=[
+                    "Off-hire", "ECA Transit", "Fuel Changeover", "Stoppage",
+                    "Deviation - Heavy weather", "Deviation - SAR operation",
+                    "Deviation - Navigational area Warning", "Deviation - Med-evac",
+                    "Deviation - Others", "Transiting Special Area - JWC area",
+                    "Transiting Special Area - IWL", "Transiting Special Area - ICE regions",
+                    "Transiting Special Area - HRA"
+                ],
             ),
             "Start Date Time": st.column_config.DatetimeColumn(
                 "Start Date Time", format="DD/MM/YYYY HH:mm", step=60
