354eb3948c2227d4cb713c3970c67e7a5e95fc78
tarunagarwal1981 <49944511+tarunagarwal1981@users.noreply.github.com>
2024-08-13 12:30:46 +0530

    Update test1.py

--- pages/test1.py
+++ pages/test1.py
@@ -16,7 +16,7 @@ def create_voyage_timeline(vessel_info, voyage_info, segment_info):
 
     # Voyage Info
     st.write(f"**Voyage ID:** {voyage_info['voyageId']} | **{segment_info['vesselCondition']}** | "
-             **CP Speed:** {segment_info['speedOrder']} knots | "
+             f"**CP Speed:** {segment_info['speedOrder']} knots | "
              f"**CP Consumption:** {segment_info['cpConsumption']} mT/day")
 
     # Timeline
