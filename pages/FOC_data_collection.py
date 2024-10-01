c1dedad15d71ba46d7a63fbd990253bf4b610f7a
tarunagarwal1981 <49944511+tarunagarwal1981@users.noreply.github.com>
2024-10-01 13:31:26 +0530

    Update FOC_data_collection.py

--- pages/FOC_data_collection.py
+++ pages/FOC_data_collection.py
@@ -422,10 +422,13 @@ def main():
     # Display corresponding report based on the selected view
     if fuel_type_view:
         display_fuel_consumption_report(bunker_survey, bunkering_happened, debunkering_happened)
+        display_additional_table(fuel_type_view)
     elif bdn_view:
         display_bdn_consumption_report(bunker_survey, bunkering_happened, debunkering_happened)
+        display_additional_table(fuel_type_view)
     elif flowmeter_method:
         display_flowmeter_method_report(bunker_survey, bunkering_happened, debunkering_happened)
+        display_additional_table(fuel_type_view)
 
     # Submit button
     if st.button("Submit Report", type="primary"):
